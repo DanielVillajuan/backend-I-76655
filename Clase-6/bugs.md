d00926e163327ef3518382b759a69ce595a0c1db
Daniel Villajuan <danielvillajuan@hotmail.com>
2025-11-04 21:06:07 -0300

    Fin de clase 6

---
+++ Clase-6/bugs.md
@@ -0,0 +1,17 @@
+1- Todo en una linea [x]
+2- Usuario ve dos veces los mensaje, o todos previos [x]
+3- Un usuario no logra recuperar el chat cuando se conecta [x]
+
+--- MEJORA ---
+
+PASAR DE VARIABLE A ARCHIVOS!
+EL usuario que envia un chat debera de identificarse visualmente en su pantalla como el prefijo "Yo:"
+No mostrar el chat hasta que se identifique el usuario
+Mostrar mensaje de bienvenida + username
+
+
+
+Mostrar usuarios conectados
+ - Identificar con verde para conectados, rojo para los que ya no estan.
+ - Roles - Admin, invitado, premium, basico, top 3 donares.
+ - Permitir emojis a usuarios premium
---
+++ Clase-6/src/app.js
@@ -0,0 +1,41 @@
+import express from 'express'
+import UserRoute from './routes/user.route.js'
+import PetsRoute from './routes/pets.route.js'
+import ViewRoutes from './routes/view.route.js'
+import path from 'node:path'
+import handlebars from 'express-handlebars'
+import { Server } from 'socket.io'
+
+const app = express()
+
+app.engine('handlebars', handlebars.engine()) // defino a express que va usar el motor de plantilla con la extension handlebars
+app.set('views', path.join(process.cwd(),'src','views')) // definimos que las vistas estaran en esta ruta src/views asi handlebars sabra donde buscarlas
+app.set('view engine', 'handlebars')
+
+// si al use no le especifico la ruta como primer parametro, lo aplicara en '/'
+app.use(express.static(path.join(process.cwd(), 'src','public'))) // 'c:/userpc/documents/capertacoder/clase-5/src/public
+
+// si le especifico a use el primer parametro, lo aplicara siempre a la misma
+app.use('/api/user', UserRoute)
+app.use('/api/pets', PetsRoute)
+app.use('/', ViewRoutes)
+
+const serverHttp = app.listen(8080, () => {
+    console.log("Server ON")
+})
+
+const serverSocket = new Server(serverHttp)
+
+const BBDD = [];
+
+serverSocket.on('connection', (socket) => {
+    console.log('Nuevo cliente conectado con id ->', socket.id )
+
+    socket.emit('lista_de_mensaje_actualizada',BBDD);
+
+    socket.on('mensaje', (payload) => { // payload -> {user, mensaje}
+        BBDD.push(payload)
+        serverSocket.emit('lista_de_mensaje_actualizada', BBDD)
+    })
+
+})
